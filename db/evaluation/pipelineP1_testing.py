63ee89d0883be4b41308459f8de3aa6bc24cc754
Robert Luis <robert.l.bluemel@gmail.com>
2021-06-05 14:31:37 +0200

    fixing transacion input

---
+++ db/evaluation/pipelineP1_testing.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from bitcoin_rpc import * 
+import json
+from kafka import KafkaProducer
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+producer = KafkaProducer(value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+startTime = time.time()
+
+for blockheight in range(1,11):
+    # retrival of block + transfer to respective topic 
+    blockhash = getBlockHash(blockheight)
+    data, block = getblock(blockhash)
+    producer.send('blocks', data)
+    #print("Error handling block at length" + blockheight)
+
+    try:
+        # retrival of transactions + transfer to respective topic
+        for txid in block['tx'][:]:
+            tx = gettx(txid,block)
+            producer.send('transactions', str(tx))
+    except:
+        print("Error handling transactions at block" + str(blockheight) + ' with transaction id' + str(txid))
+
+
+endTime = (time.time() - startTime)
+print('Execution time in seconds: ' + str(endTime))
