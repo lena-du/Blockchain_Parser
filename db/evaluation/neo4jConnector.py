876b4f0c41e498e1ea585eae06455b4b9d9e6c32
Robert Luis <robert.l.bluemel@gmail.com>
2021-06-17 17:51:21 +0200

    changed path reference

--- db/evaluation/neo4jConnector.py
+++ db/evaluation/neo4jConnector.py
@@ -5,13 +5,20 @@
 
 from neo4j import GraphDatabase
 import pandas as pd
+import os
 
 
-def startNeo4jSession(path_to_credentisls = '../credentialsNeo4j.json',
+def startNeo4jSession(credentials  = './credentialsNeo4j.json',
                       location = 'server',   # create settings file, retrieve from these
                       port = 7687):          # same here
     
-    credentialsNeo4j = pd.read_json(path_to_credentisls)
+    try:
+        credentialsNeo4j = pd.read_json(credentials)
+    except:
+        try:
+            credentialsNeo4j = pd.read_json(os.path.join(os.pardir, credentials))
+        except:
+            raise Exception('credentials could not be found')
 
     uri = "neo4j://localhost:" + str(port)
 
